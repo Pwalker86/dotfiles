8fe74262c253007e5468fce5418c1a8ee34d793c
Phil Walker <phil1586@gmail.com>
2024-12-03 01:09:20 -0700

    add ale, but keep it commented for now

---
+++ lua/philw/plugins/ale.lua
@@ -0,0 +1,16 @@
+return {
+    -- 'dense-analysis/ale',
+    -- config = function()
+    --     -- Configuration goes here.
+    --     local g = vim.g
+    --
+    --     g.ale_ruby_rubocop_auto_correct_all = 1
+    --
+    --     g.ale_linters = {
+    --         ruby = {'rubocop', 'ruby'},
+    --     }
+    --
+    --     g.ale_lint_on_text_changed='never'
+    --     g.ale_fix_on_save = true
+    -- end
+}
--- lua/philw/plugins/nvim-cmp.lua
+++ lua/philw/plugins/nvim-cmp.lua
@@ -13,6 +13,7 @@ return {
     local luasnip = require("luasnip")
 
     require("luasnip.loaders.from_vscode").lazy_load()
+    require'luasnip'.filetype_extend("ruby", {"rails"})
 
     cmp.setup({
       completion = {

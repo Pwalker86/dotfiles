2b3802a13d690ee8c652e701ce082fc179bf017e
Phil Walker <phil1586@gmail.com>
2025-05-05 21:35:14 -0600

    only use oil in a float

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,29 +1,29 @@
 return {
-    "stevearc/oil.nvim",
-    opts = {
-      default_file_explorer = true,
-      columns = {
-        "icon",
-        "permissions",
-        "size",
-        "mtime",
-      },
-      float = {
-        padding = 2,
-        max_width = 80,
-        max_height = 30,
-      },
-      keymaps = {
-        ["q"] = "actions.close",
-        ["<C-c>"] = "actions.close",
-        ["<CR>"] = "actions.select",
-        ["<C-s>"] = "actions.select_split",
-        ["<C-v>"] = "actions.select_vsplit",
-      },
-    },
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>e", "<cmd>Oil<CR>", desc = "Open Oil file explorer" },
-      { "<leader>E", "<cmd>Oil --float<CR>", desc = "Open Oil in float" },
-    },
-  }
+	"stevearc/oil.nvim",
+	opts = {
+		default_file_explorer = true,
+		columns = {
+			"icon",
+			"permissions",
+			"size",
+			"mtime",
+		},
+		float = {
+			padding = 2,
+			max_width = 80,
+			max_height = 30,
+		},
+		keymaps = {
+			["q"] = "actions.close",
+			["<C-c>"] = "actions.close",
+			["<CR>"] = "actions.select",
+			["<C-s>"] = "actions.select_split",
+			["<C-v>"] = "actions.select_vsplit",
+		},
+	},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	keys = {
+		-- { "<leader>e", "<cmd>Oil<CR>", desc = "Open Oil file explorer" },
+		{ "<leader>o", "<cmd>Oil --float<CR>", desc = "Open Oil in float" },
+	},
+}
